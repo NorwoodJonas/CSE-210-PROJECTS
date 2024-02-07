b99a0bf2b99dcb68de631eb13b356393864af072
Jonas Joestar <jonasn@byui.edu>
2024-02-07 08:59:19 -0700

    Adding untracked documents

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,16 +1,26 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Journal ofCourse = new Journal();
-        Menu whatever = new Menu("Journal Program 1.29.86", ofCourse);
-        do
-        {
-            whatever.ShowMenu();
-        }
-        while(whatever.Option()); //In theory it should run Option every time
-        return;
+        Console.WriteLine("FractionalClassTesting --- Version 0.000.11");
+        Factions one = new(1);
+        Factions definaultly = new();
+        Factions f = new(2, 3);
+
+        Console.WriteLine(one.FrackingTwoSeams());
+        Console.WriteLine(one.ActionIntoBubble());
+        Console.WriteLine(definaultly.FrackingTwoSeams());
+        Console.WriteLine(definaultly.ActionIntoBubble());
+        Console.WriteLine(f.FrackingTwoSeams());
+        Console.WriteLine(f.ActionIntoBubble());
+
+        Console.WriteLine("changing things");
+
+        f.DenominicronChange(7);
+        f.NumeroatorChange(4);
+
+        Console.WriteLine(f.NumbervatorGet());
+        Console.WriteLine(f.TheNotMeNotHer____Git());
+        Console.WriteLine(f.FrackingTwoSeams());
     }
 }
