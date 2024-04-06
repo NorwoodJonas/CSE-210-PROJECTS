e7eeebfde2415327a2e36a6282d4f56ffb0e8432
Jonas Joestar <jonasn@byui.edu>
2024-04-06 10:54:15 -0600

    Almost done with the function that allows you to move from one area to another.

--- final/FinalProject/Commanding.cs
+++ final/FinalProject/Commanding.cs
@@ -1,28 +1,32 @@
+using System.Diagnostics.Contracts;
+
 class Commanding
 {
     Dictionary<string, Delegate> functional;
     WordReference dict;
     World worldly;
+    Dictionary<string, string[]> neededThings;
+
+    private delegate string[] Why(bool x);
 
     public Commanding(WordReference dic, World worldd)
     {
         dict = dic;
         worldly = worldd;
-        functional.Add("go", ChangeArea);
+        functional.Add("go", Go);
+        string[] t = {"place"};
+        neededThings.Add("go", t); //I wish c# would let me use curly brackets to define string arrays anywhere
     }
 
-    public void Go(string[] input) //all functions should be this way
+    public void Go(List<string> input) //all functions should be this way
     {
         //Check to see if any other part of the string is in the area
         foreach(GenericThing g in worldly.activeArea.thingsInArea)
         {
-            foreach(string s in input)
+            if(g.GetName() == input[0])
             {
-                if(g.GetName() == s)
-                {
-                    ChangeArea(g.AreaNum());
-                    return;
-                }
+                ChangeArea(g.AreaNum());
+                return;
             }
         }
         Console.WriteLine($"You fail to {0} anywhere", input[0]); //you fail to go anywhere, etc. Could be done better
@@ -42,10 +46,25 @@ class Commanding
             temp++;
         }
         //Try finding other words associated with that action
-        
-        //Guess object in area associated with nouns
+        string[] wordsToFind = neededThings[input[actionNum]];
+        List<string> arguably = new();
+        foreach(string s in wordsToFind)
+        {
+            temp = 0;
+            foreach(string b in wordType)
+            {
+                if(s == wordType[temp])
+                {
+                    arguably.Add(input[temp]);
+                }
+                temp++;
+            }
+        }
+        //I was going to overload the method but delegates don't like method groups so... IDK
+        //I need a list of word types I need with each command. Done.
+        //Guess object in area associated with nouns, only if you need nouns though
         //Call function with associated words
-        //Call function in class based on input, I don't want to use a bunch of if-else statements, use invoke
+        functional[input[actionNum]](arguably); //How to properly invoke these delegates?
     }
     //Make all of the commands that affect things here, things like items, spells, characters, and such will call commands here
 
