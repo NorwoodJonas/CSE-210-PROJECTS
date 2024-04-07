ce306c27cb2fcd8a3e6bd388e61e336643148a6f
Jonas Joestar <jonasn@byui.edu>
2024-04-07 10:03:07 -0600

    Okay guys, so I just allowed GenericThings to acces commands, this means there is a specific way in which you need to instantiate and mess with objects. If only global varibles were a thing in C#

--- final/FinalProject/Character.cs
+++ final/FinalProject/Character.cs
@@ -1,7 +1,7 @@
 class Character : GenericThing //Why is a character a GenericThing? So that I can keep all of them in the same array ofCourse().
 {
 
-    public Character()
+    public Character(Commanding c) : base(c)
     {
 
     }
--- final/FinalProject/Portal.cs
+++ final/FinalProject/Portal.cs
@@ -1,8 +1,13 @@
 class Portal : GenericThing
 { //A GenericThing that when used will move player to a different area
     public int areaName;
+
+    public Portal(Commanding c) : base(c)
+    {
+
+    }
     public override void Use()
     {
-        
+        commands.ChangeArea(areaName);
     }
 }
