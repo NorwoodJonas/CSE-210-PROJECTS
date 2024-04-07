e8bf253dc5b03374688175b661c1f8773b7e27a5
Jonas Joestar <jonasn@byui.edu>
2024-04-06 22:39:52 -0600

    IDK

--- final/FinalProject/Jewerly.cs
+++ final/FinalProject/Jewerly.cs
@@ -1,20 +1,31 @@
+using System.Diagnostics.Tracing;
+using System.Security.AccessControl;
+
 class Jewerly : GenericThing
 { //A GenericThing that adjusts character stats
     List<string> usefulness;
     Random ran = new();
+    private int level;
 
-    public Jewerly()
+    public Jewerly(int l)
     {
         usefulness.Add($"The {name} shines with brilliant lustre...");
         usefulness.Add($"You can't helped but be entranced by the {name}");
         usefulness.Add($"The {name} tempts you");
         usefulness.Add($"I paid too much for this {name}");
         usefulness.Add($"I think this {name} is fake");
+        usefulness.Add($"The {name} gives you 14k worth of vision... supposedly.");
+        level = l;
     }
 
     public override void Use()
     {
         int index = ran.Next(0, usefulness.Count);
         Console.WriteLine(usefulness[index]);
+    }
+
+    public int Held()
+    {
+        return level;
     }
 }
--- final/FinalProject/Portal.cs
+++ final/FinalProject/Portal.cs
@@ -1,4 +1,4 @@
 class Portal : GenericThing
 { //A GenericThing that when used will move player to a different area
-
+    public int areaName;
 }
