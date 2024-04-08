8297584eda37aad04264243fa64fc699f3d7389f
Jonas Joestar <jonasn@byui.edu>
2024-04-07 22:19:52 -0600

    Added a comment for clearer understanding of code

--- final/FinalProject/World.cs
+++ final/FinalProject/World.cs
@@ -1,7 +1,7 @@
 class World
 { //should contain a list of areas, and things affecting area
     private int time = 0; //time, goes between cycles of 86,400 per day
-    private List<Area> areas = new();
+    private List<Area> areas = new(); //Because of the way c# works, _underscoreCamelCase isn't that useful
     public Area activeArea; //Look, I know that you want this to be private, but a function to get active area would just be worse.
 
     public Character pc; //need to fix this
